9e18424b422126090bc5ff24e073b790eca0ca76
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-12-21 15:04:37 +0100

    Rework and improve test index.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,23 +1,23 @@
-import {start,StartMode,Config}                           from 'zation-server';
-import {when,describe,create,before,after,ClientOptions}  from 'zation-assured';
-import StarterConfig                                      from './../src/configs/starter.config';
+import {start,StartMode,Config} from 'zation-server';
+import {ClientOptions}  from 'zation-assured';
 
-const TEST_PORT = 3008;
+import "../src/configs/app.config.ts";
+import "../src/configs/server.config.ts";
+
+const TEST_PORT = 3011;
 
 export const clientConfig : ClientOptions = {
-    port : TEST_PORT,
-    system : 'T',
-    version : 4.5
+    port: TEST_PORT,
+    system: 'T',
+    version: 4.5
 };
 
-before(async () => {
-    await start(Config.merge(Config.starterConfig({
-        port : TEST_PORT,
-        hostname : 'localhost',
-        origins : '*:*',
-        workers : 1,
-        brokers : 1,
-        debug : false,
-    }),StarterConfig),StartMode.Test);
-});
+Config.serverConfig({
+    port: TEST_PORT,
+    hostname: 'localhost',
+    origins: '*:*',
+    debug: false,
+}).register(true);
+
+before(async () => start(Config.configurations,StartMode.Test));
 
