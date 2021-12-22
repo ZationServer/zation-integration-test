4113dad6a16108eb825e0d94a04cabb333f3bf2d
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-12-22 01:05:23 +0100

    Update app.config.ts

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -6,22 +6,31 @@ Copyright(c) Ing. Luca Gian Scaringella
 
 import {Config}          from 'zation-server';
 
-import "../controllers/auth/logIn.ts";
-import "../controllers/auth/secret.ts";
-import "../controllers/validation/canBeNull.ts";
-import "../controllers/validation/stringValidation.ts";
-import "../controllers/validation/anyOfValidation.ts";
-import "../controllers/validation/objectValidation.ts";
-import "../controllers/validation/objectClassValidation.ts";
-import "../controllers/validation/tupleObjectValidation.ts";
-import "../controllers/validation/arrayValidation.ts";
-import "../controllers/validation/allAllow.ts";
-import "../controllers/validation/nothingAllowed.ts";
-import "../controllers/apiLevel/apiLevel.ts";
-import "../controllers/timeout/timeout.ts";
-import "../controllers/validation/complexValidation.ts";
-import "../controllers/validation/extendValueModel.ts";
-import "../controllers/modelExtra/modelExtra.ts";
+import "../api/controllers/auth/logIn.ts";
+import "../api/controllers/auth/secret.ts";
+import "../api/controllers/validation/canBeNull.ts";
+import "../api/controllers/validation/stringValidation.ts";
+import "../api/controllers/validation/anyOfValidation.ts";
+import "../api/controllers/validation/objectValidation.ts";
+import "../api/controllers/validation/objectClassValidation.ts";
+import "../api/controllers/validation/tupleObjectValidation.ts";
+import "../api/controllers/validation/arrayValidation.ts";
+import "../api/controllers/validation/allAllow.ts";
+import "../api/controllers/validation/nothingAllowed.ts";
+import "../api/controllers/apiLevel/apiLevel.ts";
+import "../api/controllers/timeout/timeout.ts";
+import "../api/controllers/validation/complexValidation.ts";
+import "../api/controllers/validation/extendValueModel.ts";
+import "../api/controllers/modelExtra/modelExtra.ts";
+import "../api/channels/chat";
+import "../api/channels/news";
+import "../api/receivers/publishMessage";
+import "../api/receivers/publishNews";
+import "../api/receivers/updateUserAge";
+import "../api/receivers/deleteProduct";
+import "../api/receivers/addProduct";
+import "../api/databoxes/userProfile";
+import "../api/databoxes/products";
 
 Config.appConfig(
     {
