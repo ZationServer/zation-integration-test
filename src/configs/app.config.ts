db7485a1ecffa212e3af7f0ef6c3f03faf32c873
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-12-23 23:50:57 +0100

    Update app.config.ts

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -4,24 +4,24 @@ GitHub: LucaCode
 Copyright(c) Ing. Luca Gian Scaringella
  */
 
-import {Config}          from 'zation-server';
+import {Config} from 'zation-server';
 
-import "../api/controllers/auth/logIn.ts";
-import "../api/controllers/auth/secret.ts";
-import "../api/controllers/validation/canBeNull.ts";
-import "../api/controllers/validation/stringValidation.ts";
-import "../api/controllers/validation/anyOfValidation.ts";
-import "../api/controllers/validation/objectValidation.ts";
-import "../api/controllers/validation/objectClassValidation.ts";
-import "../api/controllers/validation/tupleObjectValidation.ts";
-import "../api/controllers/validation/arrayValidation.ts";
-import "../api/controllers/validation/allAllow.ts";
-import "../api/controllers/validation/nothingAllowed.ts";
-import "../api/controllers/apiLevel/apiLevel.ts";
-import "../api/controllers/timeout/timeout.ts";
-import "../api/controllers/validation/complexValidation.ts";
-import "../api/controllers/validation/extendValueModel.ts";
-import "../api/controllers/modelExtra/modelExtra.ts";
+import "../api/controllers/auth/logIn";
+import "../api/controllers/auth/secret";
+import "../api/controllers/validation/canBeNull";
+import "../api/controllers/validation/stringValidation";
+import "../api/controllers/validation/anyOfValidation";
+import "../api/controllers/validation/objectValidation";
+import "../api/controllers/validation/objectClassValidation";
+import "../api/controllers/validation/tupleObjectValidation";
+import "../api/controllers/validation/arrayValidation";
+import "../api/controllers/validation/allAllow";
+import "../api/controllers/validation/nothingAllowed";
+import "../api/controllers/apiLevel/apiLevel";
+import "../api/controllers/timeout/timeout";
+import "../api/controllers/validation/complexValidation";
+import "../api/controllers/validation/extendValueModel";
+import "../api/controllers/modelExtra/modelExtra";
 import "../api/channels/chat";
 import "../api/channels/news";
 import "../api/receivers/publishMessage";
